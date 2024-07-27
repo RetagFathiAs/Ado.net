a642d517cb6d09541eb3ade37fda40c9e637ebad
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-07-27 23:38:45 +0200

    Add files via upload

---
+++ crystall/Form2.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports System.Linq
+Imports System.Runtime.CompilerServices
+Imports System.Windows.Forms
+
+
+Public Class Form2
+
+  Public texts As String
+
+
+  Private Sub addbtn_Click(sender As Object, e As EventArgs) Handles addbtn.Click
+    texts = searchtext.Text
+    nametxet.Text = texts
+
+    Me.Refresh()
+  End Sub
+  Public Function sett()
+
+    Dim en As String = Me.searchtext.Text
+
+    Return (en)
+  End Function
+
+  Private Sub delbtn_Click(sender As Object, e As EventArgs)
+
+
+  End Sub
+
+  Private Sub codingbtn_Click(sender As Object, e As EventArgs) Handles codingbtn.Click
+
+
+
+    Dim cr As New crystal_coding()
+    cr.Show()
+  End Sub
+
+  Private Sub noncodingbtn_Click(sender As Object, e As EventArgs) Handles noncodingbtn.Click
+
+    Dim f As New form
+    f.Show()
+  End Sub
+
+  Private Sub searchbtn_Click(sender As Object, e As EventArgs)
+
+  End Sub
+
+  Private Sub nametxet_Click(sender As Object, e As EventArgs) Handles nametxet.Click
+
+  End Sub
+End Class
---
+++ crystall/crystal_coding.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Data.OleDb
+
+
+Public Class crystal_coding
+  Dim conn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\test\Desktop\crystall\db2.accdb;Persist Security Info=False")
+  Dim foryy As String
+
+
+  Dim myform As New Form2
+
+  Dim res As String
+  Private Sub crystal_coding_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    myform = Form2.ActiveForm
+
+    Dim query As String = "SELECT * FROM tb1 WHERE city='" & myform.sett() & "';"
+    Dim dataAdapter As New OleDbDataAdapter(query, conn)
+    Dim ds As New DataSet1
+    dataAdapter.Fill(ds.tb1)
+
+      Dim report As New CrystalReport2
+      report.SetDataSource(ds)
+    CrystalReportViewer1.ReportSource = report
+
+  End Sub
+End Class
