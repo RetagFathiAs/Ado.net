c922b89a0d4d4b479f0a7aca59efb72db4851cdb
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-07-27 23:57:04 +0200

    Update crystal_coding.vb

--- crystall/crystal_coding.vb
+++ crystall/crystal_coding.vb
@@ -1,9 +1,9 @@
-﻿Imports System.Data.OleDb
+Imports System.Data.OleDb
 
 
 Public Class crystal_coding
   Dim conn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\test\Desktop\crystall\db2.accdb;Persist Security Info=False")
-  Dim foryy As String
+
 
 
   Dim myform As New Form2
@@ -12,7 +12,7 @@ Public Class crystal_coding
   Private Sub crystal_coding_Load(sender As Object, e As EventArgs) Handles MyBase.Load
     myform = Form2.ActiveForm
 
-    Dim query As String = "SELECT * FROM tb1 WHERE city='" & myform.sett() & "';"
+    Dim query As String = "SELECT * FROM tb1 WHERE city='" & myform.searchtext.Text & "';"
     Dim dataAdapter As New OleDbDataAdapter(query, conn)
     Dim ds As New DataSet1
     dataAdapter.Fill(ds.tb1)
